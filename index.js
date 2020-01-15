59e840901f534c1a797328b28d469d5fb752df90
englishman003 <ncluff003@gmail.com>
2020-01-14 20:15:21 -0500

    first answer done

--- index.js
+++ index.js
@@ -39,9 +39,16 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
+function makePersonObject(Id, Name, Email) {
   /* code here */
+  return{
+      id: Id,
+      name: Name,
+      email: Email
+  }
+  
 }
+makePersonObject(5, 'leia', 'leia@leia.com');
 
 /**
  * ### Challenge `getName`
